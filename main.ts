d708d8f8f9cbfe56384bf5f444a4cfdd4a0875bd
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-09-20 15:55:27 -0400

    Typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael B
+ * Created on: Sep 2023
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(100)
+
+basic.showString(('A rectangle has dimensions 5 cm & 3 cm. The perimeter would be:') + (2 * (5 + 3)).toString() + ('cm.') + ('The area would be:') + (5 * 3).toString() + ('cm^2.'))
