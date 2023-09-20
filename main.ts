e0d0cd5c5cb79127f2ab3d2221e79ecdab57329d
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-09-20 15:23:27 -0400

    Restore '2023-09-20, 1:25:38 p.m. fixed'

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael B
+ * Created on: Sep 2023
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString(('A rectangle has dimensions 5 cm & 3 cm The perimeter would be:') + (2 * (3 + 5)).toString() + ('cm') + ('Thr area would be:') + (3 * 5).toString() + ('cm^2'))
